83ee49e10405f84781a6076019cc69f0358f0d77
Nmeendezz <nicolasmarin2006@hotmail.com>
2025-10-05 17:18:24 +0200

    ejercicio 10

--- ejerciciosArray.php
+++ ejerciciosArray.php
@@ -10,7 +10,7 @@
 <body>
     <?php
     //Ejercicio 9
-    
+    /*
     $palos = ["Oros", "Copas", "Espadas", "Bastos"];
 
     $numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -32,17 +32,62 @@
 
     $sum = 0;
     for ($i = 0; $i < 10; $i++) {
-        $paloRandom = $palos[rand(0, 3)];
+        $paloRandom = $palos[mt_rand(0, 3)];
         $numRandom = $numeros[mt_rand(0, 11)];
-        foreach ($puntos as $carta => $punto) {
-            if ($numRandom == $carta) {
+        foreach ($puntos as $num => $punto) {
+            if ($numRandom == $num) {
                 $sum += $punto;
                 echo "<p>$numRandom de $paloRandom que equivale a $punto puntos</p>";
             }
         }
     }
-    echo "$sum";
+    echo "Su puntaje es de $sum";
+    */
 
+    //Ejercicio 10
+    
+    $palos = ["Oros", "Copas", "Espadas", "Bastos"];
+
+    $numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    $puntos = [
+        1 => 11,
+        2 => 0,
+        3 => 10,
+        4 => 0,
+        5 => 0,
+        6 => 0,
+        7 => 0,
+        8 => 0,
+        9 => 0,
+        10 => 2,
+        11 => 3,
+        12 => 4,
+    ];
+
+    $baraja = [];
+    $sum = 0;
+    for ($i = 0; $i < 10; $i++) {
+        $paloRandom = $palos[mt_rand(0, 3)];
+        $numRandom = $numeros[mt_rand(0, 11)];
+
+        $baraja = [$numRandom => $paloRandom];
+        $baraja = "$numRandom de $paloRandom";
+        echo "<p>$baraja</p>";
+
+    }
+
+    foreach ($puntos as $num => $punto) {
+        if ($numRandom == $num) {
+            $sum += $punto;
+            for ($i = 0; $i < count($baraja); $i++) {
+                echo "<p>$baraja que equivale a $punto puntos</p>";
+            }
+        }
+    }
+
+
+    echo "Su puntaje es de $sum";
 
     ?>
 </body>
